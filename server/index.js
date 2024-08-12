97de8355b9339b9a641915d4ede0df29fcfcf74e
topcheto356 <miro.kurtev27@gmail.com>
2024-08-12 14:10:07 +0300

    add nodejs server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const port = 3003;
+const app = express();
+
+const logger = function (req, res, next) {
+  console.log(`[${Date.now()}] ${req.method} ${req.url}`);
+
+  next();
+};
+app.use(logger);
+
+app.get('/test', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.get('/greet/:name', (req, res) => {
+  res.json({ greeting: `Yooo ${req.params.name}` });
+});
+
+app.listen(port, () => console.log(`Server is now listening to port: ${port}`));
