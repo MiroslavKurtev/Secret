58038cab6f7093b420af65fe0ef962523b11b063
topcheto356 <miro.kurtev27@gmail.com>
2024-08-12 15:26:41 +0300

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import Login from './views/Login/Login';
 import Register from './views/Register/Register';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+
+    // with this data loaded before rendering
+    // loader: async ({ request, params }) => {
+    //   return fetch(`/fake/api/teams/${params.teamId}.json`, {
+    //     signal: request.signal,
+    //   });
+    // },
+
+    // performing this mutation when data is submitted to it
+    // action: async ({ request }) => {
+    //   return updateFakeTeam(await request.formData());
+    // },
+
+    // and renders this element in case something went wrong
+    errorElement: <div>Error 404 Not found</div>,
+  },
+]);
+
 function App() {
-  return (
-    <div className='Auth'>
-      <Login />
-      <Register />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
