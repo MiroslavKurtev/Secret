abae2e280f5dd1722da224206a58ca2fdf5a8e53
topcheto356 <miro.kurtev27@gmail.com>
2024-08-14 15:29:26 +0300

    ranaming

--- src/views/Profile/ProfileWorkout.jsx
+++ src/views/Profile/ProfileWorkout.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { TableCell, TableRow } from '../../components/Table';
-import { formatSeconds } from '../../helpers/formatTime.js';
+import { formatSeconds } from '../../utils/formatTime.js';
 
 const ProfileWorkout = ({ workout, count }) => {
-	return (
-		<TableRow>
-			{Object.entries(workout).map(([key, value]) =>
-				key !== 'id' ? (
-					<TableCell key={'w-' + workout.id + key}>
-						{key === 'time' || key === 'time_first'
-							? formatSeconds(value)
-							: value}
-					</TableCell>
-				) : (
-					<TableCell key={'w-' + workout.id + key}>{count + 1}</TableCell>
-				)
-			)}
-			<TableCell key={'w-' + workout.id + 'link'}>Link</TableCell>
-		</TableRow>
-	);
+  return (
+    <TableRow>
+      {Object.entries(workout).map(([key, value]) =>
+        key !== 'id' ? (
+          <TableCell key={'w-' + workout.id + key}>
+            {key === 'time' || key === 'time_first'
+              ? formatSeconds(value)
+              : value}
+          </TableCell>
+        ) : (
+          <TableCell key={'w-' + workout.id + key}>{count + 1}</TableCell>
+        )
+      )}
+      <TableCell key={'w-' + workout.id + 'link'}>Link</TableCell>
+    </TableRow>
+  );
 };
 
 export default ProfileWorkout;
